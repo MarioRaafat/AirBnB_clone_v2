6674890d1fb9e39031b6a3225e0594fd4951d518
MarioRaafat <mariorafat10@gmail.com>
2024-07-09 08:20:43 -0700

    task0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,10 +5,10 @@ The application listens on 0.0.0.0, port 5000.
 Routes:
     /: Displays 'Hello HBNB!'
 """
-
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
